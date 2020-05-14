e71184c0db3adb997929fcd93f2222156f28390d
LynnAmsbury <Lynn8542@gmail.com>
2020-05-14 13:32:22 -0600

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+# number should only be divisible by 1 and itself 
+def prime?(integer)
+    if integer <= 1
+      return false
+    else
+      i = 2
+      while i < integer - 1
+        if integer % i == 0
+          return false
+        end
+        i += 1
+      end
+      return true
+    end
+  end
